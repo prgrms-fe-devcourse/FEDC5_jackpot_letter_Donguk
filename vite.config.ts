1222275a4de82ffef6c9ad355a067340a95ac2ed
kimyou1102 <yoosk5485@naver.com>
2023-12-26 14:51:56 +0900

    🔧setting: 절대경로 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '@components', replacement: resolve(__dirname, 'src/components') }
+    ]
+  },
+  plugins: [react(), tsconfigPaths()]
+});
