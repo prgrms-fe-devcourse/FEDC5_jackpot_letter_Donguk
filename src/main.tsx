407c6818ba991736e9756b774b6eebf8b6fbc158
kimyou1102 <yoosk5485@naver.com>
2023-12-26 11:02:46 +0900

    setting: react-router-dom 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <ReactQueryDevtools initialIsOpen={true} />
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
   </QueryClientProvider>
 );
