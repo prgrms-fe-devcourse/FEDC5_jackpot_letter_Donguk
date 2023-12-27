c0090c7d1eb230c532e837fe468e2a8d54a93914
kimyou1102 <yoosk5485@naver.com>
2023-12-28 02:15:57 +0900

    ✨feat: 마이페이지 팔로잉 팔로워 컴포넌트 ui 구현

---
+++ src/components/Mypage/Follow/index.tsx
@@ -0,0 +1,67 @@
+import { useState, useRef } from 'react';
+import { css } from '@emotion/react';
+import FollowList from '../FollowList';
+import { Container, UnderLine } from './index.style';
+
+function Follow() {
+  const [selected, setSelected] = useState('follower');
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const followListData1 = [
+    { image: '', name: '테스트1' },
+    { image: '', name: '테스트2' },
+    { image: '', name: '테스트3' }
+  ];
+
+  const handleFollowTypeClick = (type: string) => {
+    if (selected === type) return;
+    setSelected(type === 'follower' ? 'follower' : 'following');
+
+    const width = scrollRef.current?.getClientRects()[0].width;
+
+    if (scrollRef && scrollRef.current) {
+      const move = selected === 'following' ? -width! : width!;
+      scrollRef.current.scrollBy({ left: move, top: 0, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <Container>
+      <div className="follow-title-wrap">
+        <span
+          css={css`
+            color: ${selected === 'follower' ? 'black' : '#d2d2d2'};
+          `}
+          onClick={() => handleFollowTypeClick('follower')}>
+          팔로워
+        </span>
+        <span
+          css={css`
+            color: ${selected === 'following' ? 'black' : '#d2d2d2'};
+          `}
+          onClick={() => handleFollowTypeClick('following')}>
+          팔로잉
+        </span>
+      </div>
+      <UnderLine className={selected === 'following' ? 'active' : ''} />
+      <div
+        ref={scrollRef}
+        css={css`
+          overflow-x: hidden;
+        `}>
+        <div className="follow-list-container">
+          <FollowList
+            type="follower"
+            followList={followListData1}
+          />
+          <FollowList
+            type="following"
+            followList={followListData1}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default Follow;
---
+++ src/components/Mypage/FollowList/index.style.tsx
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const FollowItemList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.938rem 0px;
+
+  .follow-item {
+    display: flex;
+    align-items: center;
+
+    span {
+      flex: 1;
+      margin-left: 0.813rem;
+    }
+
+    button {
+      width: 5rem;
+      height: 2rem;
+      border-radius: 10px;
+      border: none;
+      background-color: #d9d9d9;
+      cursor: pointer;
+    }
+  }
+`;
