eac374ff9dd7af76cd2dad97b11d458d6efefb24
kimyou1102 <yoosk5485@naver.com>
2023-12-27 14:52:10 +0900

    ✨feat: modal 커스텀 훅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import { Global } from '@emotion/react';
 import reset from './styles/reset';
+import Modal from '@components/Common/Modal';
+import useModal from './hooks/useModal';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [visible, handleModalClick] = useModal();
 
   return (
     <>
       <Global styles={reset} />
-      <div>
-        <a
-          href="https://vitejs.dev"
-          target="_blank">
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+          handleModalClick(e)
+        }>
+        모달클릭
+      </button>
+      <Modal
+        children={
+          <>
+            <h1>하이</h1>
+          </>
+        }
+        width={20}
+        height={10}
+        visible={visible}
+        handleModalClose={(e: React.MouseEvent<HTMLDivElement>) =>
+          handleModalClick(e)
+        }
+      />
     </>
   );
 }
