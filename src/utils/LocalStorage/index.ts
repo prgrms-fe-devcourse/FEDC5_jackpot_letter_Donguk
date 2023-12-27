bcac1abfa9aadebedb844b868afe048d6676d248
choi-ik <ci980704@naver.com>
2023-12-27 13:03:41 +0900

    ✨Feat: LocalStorage utils 구현

---
+++ src/utils/LocalStorage/index.ts
@@ -0,0 +1,38 @@
+interface valueType<T> {
+  [key: string]: Array<T> | T;
+}
+
+interface LocalStorageProps<T> {
+  key: string;
+  value?: valueType<T>;
+  defaultValue?: valueType<T> | string;
+}
+
+const STORAGE = window.localStorage;
+
+export const getStorage = <T>({
+  key,
+  defaultValue = ''
+}: LocalStorageProps<T>): T => {
+  try {
+    const data = STORAGE.getItem(key);
+
+    if (!data) return defaultValue as T;
+
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const setStorage = <T>({ key, value }: LocalStorageProps<T>) => {
+  try {
+    STORAGE.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const removeStorage = <T>({ key }: LocalStorageProps<T>) => {
+  STORAGE.removeItem(key);
+};
