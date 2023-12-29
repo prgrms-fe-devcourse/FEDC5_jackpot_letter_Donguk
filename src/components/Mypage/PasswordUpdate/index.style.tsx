cb819707defd2047e0f5789dc6846359a3fbdf20
kimyou1102 <yoosk5485@naver.com>
2023-12-30 03:34:14 +0900

    ✨ feat: 마이페이지 PasswordUpdate 컴포넌트 구현

---
+++ src/components/Mypage/PasswordUpdate/index.style.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  height: 100%;
+  padding: 1.625rem 1.75rem;
+  box-sizing: border-box;
+
+  .name-form {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .input-wrap {
+      display: flex;
+      flex-direction: column;
+      gap: 0.8rem;
+      flex-grow: 1;
+    }
+
+    .warning-text {
+      color: red;
+      font-size: 0.75rem;
+    }
+  }
+`;
