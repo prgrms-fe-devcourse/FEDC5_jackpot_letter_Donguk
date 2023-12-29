8e2a9e7f1a0e954b348f188b4247537fb35d4b54
youkyeong60 <youkyeongs@naver.com>
2023-12-29 16:25:47 +0900

    🔧setting: import rule

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { BrowserRouter } from 'react-router-dom';
+import App from '@/App.tsx';
+import './index.css';
 
 const queryClient = new QueryClient();
 
