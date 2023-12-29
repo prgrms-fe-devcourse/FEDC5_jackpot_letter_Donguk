e6517714842e127837f3f84b9968d0dcd2397950
youkyeong60 <youkyeongs@naver.com>
2023-12-29 11:16:38 +0900

    refactor: button 타입 수정

---
+++ src/components/Common/Button/index.style.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import { buttonTypes, buttonSize } from './type';
+
+export const ButtonStyle = styled.button<{
+  disabled: boolean;
+  kind: string;
+  size: string;
+  styleOption: { [key: string]: string };
+}>`
+  font-weight: 700;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  ${({ kind }) => buttonTypes[kind]};
+  ${({ size }) => buttonSize[size]};
+  ${({ styleOption }) => styleOption}
+`;
--- src/hooks/Button/index.style.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import { buttonTypes, buttonSize } from './type';
-
-export const ButtonStyle = styled.button<{ buttonType: string; size: string }>`
-  font-weight: 700;
-  ${({ buttonType }) => buttonTypes[buttonType]};
-  ${({ size }) => buttonSize[size]};
-`;
