706598d6d4bdb2ff8b595b398d81aa8c79e103c3
imssein <hwinnie131@gmail.com>
2024-01-01 13:34:21 +0900

    🔨 refactor: 상수, validation 파일 분리

---
+++ src/constants/account.ts
@@ -0,0 +1,35 @@
+export const ACCOUNT_SIGNIN_DATA = [
+  {
+    id: 1,
+    label: '이메일',
+    value: 'email',
+    type: 'text'
+  },
+  {
+    id: 2,
+    label: '비밀번호',
+    value: 'password',
+    type: 'password'
+  }
+];
+
+export const ACCOUNT_SIGNUP_DATA = [
+  {
+    id: 1,
+    label: '이메일',
+    value: 'email',
+    type: 'text'
+  },
+  {
+    id: 2,
+    label: '비밀번호',
+    value: 'password',
+    type: 'password'
+  },
+  {
+    id: 3,
+    label: '비밀번호 확인',
+    value: 'checkPassword',
+    type: 'password'
+  }
+];
