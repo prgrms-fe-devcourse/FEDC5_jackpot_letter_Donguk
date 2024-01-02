dc9dcbb05f8fafa7f3af4621504bf8e68016606a
imssein <hwinnie131@gmail.com>
2024-01-02 12:46:29 +0900

    ✨ feat: 로그인, 회원가입 임시 페이지 구현

---
+++ src/pages/SignInPage/index.tsx
@@ -0,0 +1,11 @@
+import SignInForm from '@/components/Account/SignInForm';
+
+function SignInPage() {
+  return (
+    <>
+      <SignInForm />
+    </>
+  );
+}
+
+export default SignInPage;
---
+++ src/pages/SignUpPage/index.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from '@/components/Account/SingUpForm';
+
+function SignUpPage() {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+}
+
+export default SignUpPage;
