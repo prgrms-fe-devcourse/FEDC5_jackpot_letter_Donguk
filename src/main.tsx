23a851f0b7b393b40d4b17cc4018cc339a9ebcfa
choi-ik <ci980704@naver.com>
2024-01-03 01:27:41 +0900

    🔨refactor: post, comment 페이지 style 관련 라우팅 경로 수정 및 ThemeProvider, Global 컴포넌트 위치 App.tsx -> Main.tsx로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -6,29 +6,36 @@ import Comment from '@components/PostComment';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from '@/App.tsx';
+import { Global, ThemeProvider } from '@emotion/react';
+import reset from './styles/_reset';
+import global from './styles/global';
+import { theme } from './theme';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <ReactQueryDevtools initialIsOpen={true} />
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<App />}
-          />
-          <Route
-            path="/post"
-            element={<Post />}
-          />
-          <Route
-            path="/comment"
-            element={<Comment />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Global styles={[reset, global]} />
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={<App />}
+            />
+            <Route
+              path="/post"
+              element={<Post />}
+            />
+            <Route
+              path="/comment"
+              element={<Comment />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    </ThemeProvider>
   </QueryClientProvider>
 );
