4a6fa0050288aeca14cfca79ec51ad926cb90181
imssein <hwinnie131@gmail.com>
2024-01-02 13:16:43 +0900

    🔨 refactor: path 상수로 변경

--- src/route/AuthMiddleware.tsx
+++ src/route/AuthMiddleware.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
 import { tokenAtom } from '@/store/auth';
+import { PATH } from '../constants/path';
 
 interface AuthMiddlewareProps {
   children: ReactElement;
@@ -9,7 +10,7 @@ interface AuthMiddlewareProps {
 const AuthMiddleware = ({ children }: AuthMiddlewareProps) => {
   const storage = useAtomValue(tokenAtom);
   if (!storage) {
-    return <Navigate to="/signin" />;
+    return <Navigate to={PATH.SIGNIN} />;
   }
   return children;
 };
