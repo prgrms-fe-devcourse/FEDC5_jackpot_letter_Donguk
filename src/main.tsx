2dfb9e80ee7a2a2aa45ed0c40664620add60652c
Choi-ik <ci980704@naver.com>
2024-01-03 01:43:48 +0900

    🔨refactor: App.tsx 라우팅 수정 (#65)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Post from '@components/Post/';
-import Comment from '@components/PostComment';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from '@/App.tsx';
@@ -19,22 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <Global styles={[reset, global]} />
       <React.StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={<App />}
-            />
-            <Route
-              path="/post"
-              element={<Post />}
-            />
-            <Route
-              path="/comment"
-              element={<Comment />}
-            />
-          </Routes>
-        </BrowserRouter>
+        <App />
       </React.StrictMode>
     </ThemeProvider>
   </QueryClientProvider>
