15b03b614b8753f9c664e280fabb05e15d235a8e
imssein <hwinnie131@gmail.com>
2024-01-03 11:18:18 +0900

    🔨 refactor: 권한별 페이지 라우팅 수정

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,5 +1,14 @@
 export const PATH = {
   ROOT: '/',
   SIGNIN: '/signin',
-  SIGNUP: '/signup'
+  SIGNUP: '/signup',
+  MYPAGE: '/mypage',
+  MYPAGE_PROFILE_UPDATE: '/mypage/profile-update',
+  MYPAGE_FOLLOW: '/mypage/follow',
+  MYPGE_POST_LIST: '/mypage/post-list',
+  MYPGE_LIKE_LIST: '/mypage/like-list',
+  MYPGE_COMMNET_LIST: '/mypage/comment-list',
+  MYPGE_PASSWORD_LIST: '/mypage/password-list',
+  POST: '/post',
+  COMMENT: '/comment'
 };
