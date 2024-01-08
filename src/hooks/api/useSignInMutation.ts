1a629d2ee527e9bd1627fe21003fb9d1693cc269
kimyou1102 <yoosk5485@naver.com>
2024-01-08 11:15:22 +0900

    🔨 refactor: 로그인 성공 시 로컬스토리지에 유저 정보 추가

--- src/hooks/api/useSignInMutation.ts
+++ src/hooks/api/useSignInMutation.ts
@@ -1,17 +1,26 @@
 import { useMutation } from '@tanstack/react-query';
 import { useSetAtom } from 'jotai';
 import { postSignIn } from '@/api/member';
-import { isLoggedInAtom, tokenAtom } from '@/store/auth';
+import {
+  channelNameAtom,
+  idAtom,
+  isLoggedInAtom,
+  tokenAtom
+} from '@/store/auth';
 
 export const useSignInMutation = () => {
   const setTokenState = useSetAtom(tokenAtom);
   const setIsLoggedIn = useSetAtom(isLoggedInAtom);
+  const setIdState = useSetAtom(idAtom);
+  const setNameState = useSetAtom(channelNameAtom);
 
   const signInMutation = useMutation({
     mutationFn: postSignIn,
-    onSuccess: ({ token }) => {
+    onSuccess: ({ token, user }) => {
       setTokenState(token);
       setIsLoggedIn(true);
+      setIdState(user._id);
+      setNameState(user.fullName);
     },
     onError: () => {
       setIsLoggedIn(false);
