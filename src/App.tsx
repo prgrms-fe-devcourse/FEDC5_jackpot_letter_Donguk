73a8ecff5ff779b14e781d7f394440c8fad82e64
imssein <hwinnie131@gmail.com>
2024-01-08 23:22:14 +0900

    💄 design: 토스트 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { CSSProperties } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from '@components/Common/SignIn';
 import { authRoutes, commonRoutes, userRoutes } from './route/AppRouter';
 import AuthMiddleware from './route/AuthMiddleware';
 
+interface ToastStyleProps extends CSSProperties {
+  textAlign: 'center';
+  wordBreak: 'keep-all';
+}
+
 function App() {
-  const toastStyle = {
+  const toastStyle: ToastStyleProps = {
     fontWeight: 600,
     padding: '0.75rem 1rem',
-    marginTop: '0.5rem'
+    marginTop: '0.5rem',
+    textAlign: 'center',
+    wordBreak: 'keep-all'
   };
 
   return (
@@ -18,25 +26,21 @@ function App() {
           <Route
             path={route.path}
             element={<SignIn>{route.component}</SignIn>}
-            key={idx}
-          ></Route>
+            key={idx}></Route>
         ))}
         {userRoutes.map((route, idx) => (
           <Route
             path={route.path}
             element={<AuthMiddleware>{route.component}</AuthMiddleware>}
-            key={idx}
-          ></Route>
+            key={idx}></Route>
         ))}
         {commonRoutes.map((route, idx) => (
           <Route
             path={route.path}
             element={<>{route.component}</>}
-            key={idx}
-          ></Route>
+            key={idx}></Route>
         ))}
       </Routes>
-
       <Toaster
         toastOptions={{
           style: { ...toastStyle }
