a99182eec2a61c8a3eb69b8c869fd3ed244f3f95
youkyeong60 <youkyeongs@naver.com>
2024-01-10 21:09:28 +0900

    ✨feat: 알람 클릭 처리 함수 구현

--- src/components/Alarm/AlarmItem/index.tsx
+++ src/components/Alarm/AlarmItem/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import dayjs from 'dayjs';
 import New from '@/assets/New.svg';
 import { Notification } from '@/types/ResponseType';
@@ -6,26 +7,52 @@ import * as Style from './index.style';
 interface Prop {
   info: Notification;
 }
-const Type: { [key: number]: string } = {
-  0: 'Comment',
-  1: 'Message',
-  2: 'Follow',
-  3: 'Like'
-};
+
+interface AlarmType {
+  en: 'Comment' | 'Message' | 'Follow' | 'Like';
+  ko: string;
+  onClick: () => void;
+}
 
 function AlarmItem({ info }: Prop) {
-  const { author, createdAt, comment, message, follow, like, seen } = info;
+  const navigate = useNavigate();
+  const { author, createdAt, comment, message, follow, like, seen, post } =
+    info;
   const optionalVariables = [comment, message, follow, like];
   const optinalNumber = optionalVariables.findIndex(
     (variable) => variable !== undefined
   );
+
+  const Type: { [key: number]: AlarmType } = {
+    0: {
+      en: 'Comment',
+      ko: '댓글이 도착했어요',
+      onClick: () => navigate(`/post/${post}`)
+    },
+    1: {
+      en: 'Message',
+      ko: '메시지가 도착했어요',
+      onClick: () => navigate(`/message/${message}`)
+    },
+    2: {
+      en: 'Follow',
+      ko: '나를 팔로우하기 시작했어요',
+      onClick: () => navigate(`/message/${message}`)
+    },
+    3: {
+      en: 'Like',
+      ko: '내 포스트를 좋아해요',
+      onClick: () => navigate(`/post/${post}`)
+    }
+  };
+
   return (
-    <Style.Notification>
+    <Style.Notification onClick={Type[optinalNumber]?.onClick}>
       <Style.Content>
-        <img src={`/src/assets/${Type[optinalNumber]}.svg`} />
+        <img src={`/src/assets/${Type[optinalNumber]?.en}.svg`} />
         <div>
           <div>{author.fullName}</div>
-          <span>{Type[optinalNumber]}가 도착했어요.</span>
+          <span>{Type[optinalNumber]?.ko}</span>
         </div>
       </Style.Content>
       <Style.Info seen={seen}>
