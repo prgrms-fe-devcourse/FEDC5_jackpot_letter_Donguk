2f5742fd3400cf607ed3aa0dae04730f16377436
youkyeong60 <youkyeongs@naver.com>
2024-01-10 20:01:23 +0900

    ✨ feat: 채널 내 letterList API 연결

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import SignIn from '@components/Common/SignIn';
 import { useAtomValue } from 'jotai';
 import { theme } from '@/theme';
 import { Global } from '@emotion/react';
-import DarkMode from './components/Common/DarkMode';
 import Mypage from './pages/Mypage';
 import NotFoundPage from './pages/NotFoundPage';
 import { authRoutes, commonRoutes, userRoutes } from './route/AppRouter';
@@ -35,21 +34,18 @@ function App() {
       <Global
         styles={[reset, global(darkMode ? theme.darkTheme : theme.lightTheme)]}
       />
-      <DarkMode darkMode={darkMode} />
       <Routes>
         {authRoutes.map((route, idx) => (
           <Route
             path={route.path}
             element={<SignIn>{route.component}</SignIn>}
-            key={idx}
-          ></Route>
+            key={idx}></Route>
         ))}
         {userRoutes.page.map((route, idx) => (
           <Route
             path={route.path}
             element={<AuthMiddleware>{route.component}</AuthMiddleware>}
-            key={idx}
-          ></Route>
+            key={idx}></Route>
         ))}
         <Route
           path="/mypage"
@@ -57,22 +53,19 @@ function App() {
             <AuthMiddleware>
               <Mypage />
             </AuthMiddleware>
-          }
-        >
+          }>
           {userRoutes.mypage.map((route, idx) => (
             <Route
               path={route.path}
               element={<AuthMiddleware>{route.component}</AuthMiddleware>}
-              key={idx}
-            ></Route>
+              key={idx}></Route>
           ))}
         </Route>
         {commonRoutes.map((route, idx) => (
           <Route
             path={route.path}
             element={<>{route.component}</>}
-            key={idx}
-          ></Route>
+            key={idx}></Route>
         ))}
         <Route
           path="/*"
