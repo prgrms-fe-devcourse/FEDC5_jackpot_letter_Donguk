e1276172a6ca403f22b159c305c38bb370ebc099
choi-ik <ci980704@naver.com>
2024-01-11 13:32:23 +0900

    🔨refactor: 헤더의 유저 닉네임 텍스트 데이터 수정

--- src/components/MessageList/Header/index.tsx
+++ src/components/MessageList/Header/index.tsx
@@ -1,12 +1,14 @@
 import * as Style from './index.style';
 
-function Header() {
+interface headerProps {
+  userName: string;
+}
+function Header({ userName }: headerProps) {
   return (
     <>
       <Style.HeaderContainer>
         <Style.HeaderInnerText>
-          <Style.HeaderUserName>데이터 넘겨 받아야함</Style.HeaderUserName>의
-          메시지함
+          <Style.HeaderUserName>{userName}</Style.HeaderUserName>의 메시지함
         </Style.HeaderInnerText>
       </Style.HeaderContainer>
     </>
--- src/pages/MessageListPage/index.tsx
+++ src/pages/MessageListPage/index.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from 'react';
 import Header from '@components/MessageList/Header';
 import SearchBar from '@components/MessageList/SearchBar';
 import UserList from '@components/MessageList/UserList';
+import { useAtomValue } from 'jotai';
+import useUser from '@/hooks/api/useUser';
 import { useUserListQuery } from '@/hooks/api/useUserListDataQuery';
+import { idAtom } from '@/store/auth';
 import { User } from '@/types/ResponseType';
 
 function MessageListPage() {
+  const userId = useAtomValue(idAtom);
   const { data: userListData } = useUserListQuery(); // 전체 데이터 리스트
   const [userFilterData, setUserFilterData] = useState<User[]>();
+  const { data: userData } = useUser(userId);
 
   useEffect(() => {
     if (userListData) setUserFilterData(userListData);
@@ -15,7 +20,7 @@ function MessageListPage() {
 
   return (
     <>
-      <Header />
+      <Header userName={userData?.fullName} />
       <SearchBar
         userListData={userListData}
         setUserFilterData={setUserFilterData}
