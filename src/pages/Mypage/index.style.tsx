f06d31d88b33371514646f90175f84398d81b455
kimyou1102 <yoosk5485@naver.com>
2024-01-13 02:20:30 +0900

    💄 design: 마이페이지 반응형 수정

--- src/pages/Mypage/index.style.tsx
+++ src/pages/Mypage/index.style.tsx
@@ -1,54 +1,12 @@
 import { theme } from '@/theme';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-
-export const scrollbar = css`
-  &::-webkit-scrollbar {
-    width: 1.25rem;
-  }
-
-  &:hover {
-    ::-webkit-scrollbar-thumb {
-      width: 0.625rem;
-      background: #b7b7b7; /* 스크롤바 막대 색상 */
-    }
-  }
-`;
 
 export const Container = styled.div<{ isMypage: boolean; darkMode: boolean }>`
   height: 100svh;
-  padding: 2.5rem 1.75rem;
   display: ${(props) => (props.isMypage ? 'block' : 'flex')};
   box-sizing: border-box;
+  overflow-y: auto;
 
   background-color: ${(props) =>
     props.darkMode ? theme.darkTheme.bgColor : theme.lightTheme.bgColor};
-
-  .sidebar-container {
-    width: ${(props) => (props.isMypage ? '100%' : '18rem')};
-    overflow-y: auto;
-    margin-right: ${(props) => (props.isMypage ? '0rem' : '1.75rem')};
-    ${scrollbar}
-  }
-
-  .main {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    flex-grow: 1;
-    width: calc(100% - 18rem - 1.75rem);
-
-    .container {
-      width: 100%;
-      overflow-y: auto;
-
-      ${scrollbar}
-    }
-  }
-
-  .loading-wrap {
-    display: flex;
-    justify-content: center;
-    width: calc(100% - 18rem - 1.75rem);
-  }
 `;
--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -1,12 +1,9 @@
-import { Suspense, useEffect, useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useAtomValue, useSetAtom } from 'jotai';
-import Header from '@/components/Mypage/Header';
 import Sidebar from '@/components/Mypage/Sidebar';
-import Loading from '@/components/PostComment/Loading';
 import useUser from '@/hooks/api/useUser';
 import { ACCESS_USER_ID } from '@/constants/api';
-import { PATHNAME } from '@/constants/sidebar';
 import { darkAtom } from '@/store/theme';
 import { userAtom } from '@/store/user';
 import { getStorage } from '@/utils/LocalStorage';
@@ -14,7 +11,6 @@ import * as Style from './index.style';
 
 function Mypage() {
   const [isMypage, setIsMypage] = useState(false);
-  const [title, setTitle] = useState('');
 
   const location = useLocation();
 
@@ -30,7 +26,6 @@ function Mypage() {
 
   useEffect(() => {
     setIsMypage(pathname.replace('/mypage', '').length <= 1);
-    setTitle(PATHNAME[pathname]);
   }, [location, pathname]);
 
   useEffect(() => {
@@ -53,19 +48,6 @@ function Mypage() {
           image={userData?.image || ''}
         />
       </div>
-
-      <Suspense
-        fallback={
-          <div className="loading-wrap">
-            <Loading loadingSize={60} />
-          </div>
-        }
-      >
-        <main className="main">
-          {!isMypage ? <Header title={title} /> : null}
-          <Outlet />
-        </main>
-      </Suspense>
     </Style.Container>
   );
 }
