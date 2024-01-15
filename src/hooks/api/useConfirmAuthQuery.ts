f321577531e4dc5494256929827dacf028b9c34a
imssein <hwinnie131@gmail.com>
2024-01-15 13:35:02 +0900

    📎 chore: 불필요한 패키지, 파일 삭제

--- src/hooks/api/useConfirmAuthQuery.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getConfirmAuth } from '@/api/member';
-
-export const useConfirmAuthQuery = () => {
-  return useQuery({
-    queryKey: ['confirmAuth'],
-    queryFn: getConfirmAuth
-  });
-};
