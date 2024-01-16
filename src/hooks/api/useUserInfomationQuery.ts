845254dcbfd46ba4bad470b871ae93177a7e247b
imssein <hwinnie131@gmail.com>
2024-01-16 12:41:25 +0900

    🔨 refactor: 불필요한 파일 삭제

--- src/hooks/api/useUserInfomationQuery.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getUserInfomation } from '@/api/member';
-
-export const useUserInfomationQuery = (userId: string) => {
-  return useQuery({
-    queryKey: [userId],
-    queryFn: () => getUserInfomation(userId)
-  });
-};
