668501e48962aaf9637c5328080b3911d279d996
imssein <hwinnie131@gmail.com>
2024-01-16 19:52:04 +0900

    🐛 fix: 라우팅 수정

--- src/hooks/useIsLoggedIn.tsx
+++ src/hooks/useIsLoggedIn.tsx
@@ -1,15 +1,21 @@
 import { useEffect } from 'react';
-import { useAtom, useAtomValue } from 'jotai';
-import { idAtom, isLoggedInAtom, tokenAtom } from '@/store/auth';
+import { useAtom } from 'jotai';
+import { ACCESS_TOKEN_KEY, ACCESS_USER_ID } from '@/constants/api';
+import { isLoggedInAtom } from '@/store/auth';
+import { getStorage } from '@/utils/LocalStorage';
 
 function useIsLoggedIn() {
+  const token = getStorage(ACCESS_TOKEN_KEY);
+  const userId = getStorage(ACCESS_USER_ID);
   const [isLoggedIn, setIsLoggedIn] = useAtom(isLoggedInAtom);
-  const token = useAtomValue(tokenAtom);
-  const userId = useAtomValue(idAtom);
 
   useEffect(() => {
-    if (token && userId !== import.meta.env.VITE_ANONYMOUS_ID_KEY) {
-      setIsLoggedIn(true);
+    if (token) {
+      if (userId === import.meta.env.VITE_ANONYMOUS_ID_KEY) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
     } else {
       setIsLoggedIn(false);
     }
--- src/route/AuthMiddleware.tsx
+++ src/route/AuthMiddleware.tsx
@@ -1,18 +1,16 @@
 import { ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import useIsLoggedIn from '@/hooks/useIsLoggedIn';
-import { PATH } from '../constants/path';
+import { PATH } from '@/constants/path';
 
 interface AuthMiddlewareProps {
   children: ReactElement;
 }
-
 const AuthMiddleware = ({ children }: AuthMiddlewareProps) => {
   const isLoggedIn = useIsLoggedIn();
 
-  if (!isLoggedIn) {
-    return <Navigate to={PATH.SIGNIN} />;
-  }
+  if (!isLoggedIn) <Navigate to={PATH.SIGNIN} />;
+
   return children;
 };
 
