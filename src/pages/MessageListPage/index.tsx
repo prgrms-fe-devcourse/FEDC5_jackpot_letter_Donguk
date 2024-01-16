dcc079d3a80a9c11ec05315cc73ae0fa8c143630
choi-ik <ci980704@naver.com>
2024-01-16 17:37:22 +0900

    🔨refactor: 비회원이 post 페이지 접근 시, 비회원 토큰을 발급 받을 수 있도록 로그인 페이지로 이동

--- src/pages/MessageListPage/index.tsx
+++ src/pages/MessageListPage/index.tsx
@@ -3,10 +3,8 @@ import Header from '@components/MessageList/Header';
 import SearchBar from '@components/MessageList/SearchBar';
 import UserList from '@components/MessageList/UserList';
 import { useAtomValue } from 'jotai';
-// import { useGetMessageConversations } from '@/hooks/api/useGetMessageConversations.ts';
 import useUser from '@/hooks/api/useUser';
 import { useUserList } from '@/hooks/api/useUserList';
-import { useUserOnline } from '@/hooks/api/useUserOnline';
 import { idAtom } from '@/store/auth';
 import { darkAtom } from '@/store/theme';
 import { AuthenticationUser } from '@/types/ResponseType';
@@ -18,9 +16,6 @@ function MessageListPage() {
   const [userFilterData, setUserFilterData] = useState<AuthenticationUser[]>(); // 필터링 데이터
   const { data: userData } = useUser(userId); // userId 가 없을때 오류가 생김
 
-  const { data: userOnline } = useUserOnline();
-
-  console.log('온라인 데이터', userOnline);
   useEffect(() => {
     if (userListData) setUserFilterData(userListData);
   }, []);
