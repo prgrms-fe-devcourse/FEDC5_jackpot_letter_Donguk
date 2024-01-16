972a4ab0cc1f8920e602aad07458ead4236e9df3
youkyeong60 <youkyeongs@naver.com>
2024-01-17 08:15:20 +0900

    🐛 fix: 안읽은 알림 표시 icon 구현

--- src/components/Common/NotificationMenu/index.style.tsx
+++ src/components/Common/NotificationMenu/index.style.tsx
@@ -1,8 +1,13 @@
 import styled from '@emotion/styled';
 
-export const Button = styled.div`
+export const Button = styled.div<{ isUnread: boolean }>`
   position: absolute;
   padding: 2rem 1rem;
   right: 3rem;
   z-index: 10;
+  img {
+    position: absolute;
+    right: 1rem;
+    visibility: ${({ isUnread }) => (isUnread ? 'block' : 'hidden')};
+  }
 `;
