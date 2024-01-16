5f04ca12b007d677a7f4ccdc8d3d7da60d6960f9
choi-ik <ci980704@naver.com>
2024-01-16 12:50:23 +0900

    🔨refactor: comment 페이지 익명 회원은 댓글 삭제 및 편지 수정 삭제 불가로 수정

--- src/components/PostComment/index.tsx
+++ src/components/PostComment/index.tsx
@@ -94,6 +94,7 @@ function PostComment() {
             darkMode={darkMode}
             postId={postId}
             postDetail={postDetail}
+            userName={userName}
           />
         )}
         <Comment
